7afd967639197b02aeacb7105e7387b453b35b91
Ismail Zahee <m3talloid@gmail.com>
2015-10-12 14:07:29 +0800

    Initial Commit

---
+++ ThaanaEditText/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThaanaEditText
+//
+//  Created by Ismail Zahee on 10/12/15.
+//  Copyright © 2015 xahy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
